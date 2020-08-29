7294f14d2e0b71e1dd51849fa982469f6417c8df
Srikanth Nutigattu <srikanth.nutigattu@gmail.com>
2020-08-29 11:46:48 +0800

    rows and columns example

--- lib/main.dart
+++ lib/main.dart
@@ -14,68 +14,121 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.black26,
       ),
-      body: Center(
 
-        child: IconButton(
-          onPressed: () {
-            print('clicked');
-          },
-          icon: Icon(
-            Icons.email,
+      //Rows and Columns
+      //Column with row Example
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text('row_text_1 '),
+              Text('row_text_2'),
+            ],
           ),
-        ),
-
-        //Buttons
-        // child: RaisedButton.icon(
-        //   onPressed: () {},
-        //   icon: Icon(
-        //     Icons.airport_shuttle
-        //   ),
-        //   label: Text('icon button'),
-        //   color: Colors.blue,
-        // ),
-
-        // child: FlatButton(
-        //   child: Text('click'),
-        //   color: Colors.lightBlue,
-        //   onPressed: () {
-        //     print('you clicked!');
-        //   } ,
-        // ),
-
-        // child: RaisedButton(
-        //   child: Text('click'),
-        //   color: Colors.lightBlue,
-        //   onPressed: () {} ,
-        // ),
-
-
-        //Using Icons
-        // child: Icon(
-        //   Icons.airport_shuttle,
-        //   color: Colors.lightBlue,
-        //   size: 50.0 ,
-        // ),
-
-
-        //Local Asset after adding it to folder and pubspec
-        // child: Image.asset('assets/image1.jpg'),
-
-        // Remote image from internet
-        // child: Image.network('https://s-media-cache-ak0.pinimg.com/736x/eb/d8/1c/ebd81ca9cde854ef1a8ccca3183e915b.jpg'),
-
-        //Adding text
-        // child: Text(
-        //   'hello Ninjas',
-        //   style: TextStyle(
-        //     fontSize: 20.0,
-        //     fontWeight: FontWeight.bold,
-        //     letterSpacing: 2,
-        //     color: Colors.grey[600],
-        //     fontFamily: 'Rubik',
-        //   ),
-        // ),
+          Text('column text'),
+          Container(
+            color: Colors.red,
+            child: Text('red'),
+          ),
+          Container(
+            color: Colors.green,
+            child: Text('green'),
+          ),
+          Container(
+            color: Colors.blue,
+            child: Text('blue'),
+          ),
+        ],
       ),
+
+
+      //Row example
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   children: <Widget>[
+      //     Text('Hello world'),
+      //     FlatButton(onPressed: () {}, color: Colors.blueAccent, child: Text('click'),),
+      //     Container(color: Colors.cyan, padding: EdgeInsets.all(30.0),child: Text('inside container'),)
+      //   ],
+      // ),
+
+      //-------------------------
+      //Layout and containers
+      // body: Container(
+      //   margin: EdgeInsets.all(20.0),
+        //   padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        //   // padding: EdgeInsets.all(10.0),
+        //   color: Colors.grey[400],
+        //   child: Text('hello'),
+        // ),
+
+
+        // Buttons, Icons, Images etc
+        // body: Center(
+
+        // child: IconButton(
+          //   onPressed: () {
+          //     print('clicked');
+          //   },
+          //   icon: Icon(
+          //     Icons.email,
+          //   ),
+          // ),
+
+          //Buttons
+          // child: RaisedButton.icon(
+          //   onPressed: () {},
+          //   icon: Icon(
+          //     Icons.airport_shuttle
+          //   ),
+          //   label: Text('icon button'),
+          //   color: Colors.blue,
+          // ),
+
+          // child: FlatButton(
+          //   child: Text('click'),
+          //   color: Colors.lightBlue,
+          //   onPressed: () {
+          //     print('you clicked!');
+          //   } ,
+          // ),
+
+          // child: RaisedButton(
+          //   child: Text('click'),
+          //   color: Colors.lightBlue,
+          //   onPressed: () {} ,
+          // ),
+
+
+          //Using Icons
+          // child: Icon(
+          //   Icons.airport_shuttle,
+          //   color: Colors.lightBlue,
+          //   size: 50.0 ,
+          // ),
+
+
+          //Local Asset after adding it to folder and pubspec
+          // child: Image.asset('assets/image1.jpg'),
+
+          // Remote image from internet
+          // child: Image.network('https://s-media-cache-ak0.pinimg.com/736x/eb/d8/1c/ebd81ca9cde854ef1a8ccca3183e915b.jpg'),
+
+          //Adding text
+          // child: Text(
+          //   'hello Ninjas',
+          //   style: TextStyle(
+          //     fontSize: 20.0,
+          //     fontWeight: FontWeight.bold,
+          //     letterSpacing: 2,
+          //     color: Colors.grey[600],
+          //     fontFamily: 'Rubik',
+          //   ),
+          // ),
+      // ),
       floatingActionButton: FloatingActionButton(
         child: Text('click'),
         onPressed: () {},
