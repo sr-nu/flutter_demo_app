27715982da5492cbd2e9d1494182b62f78a0172b
Srikanth Nutigattu <srikanth.nutigattu@gmail.com>
2020-08-29 10:45:50 +0800

    made stateless widget for hot reload and examples of Image local, image network, and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Hello!"),
-      centerTitle: true,
-      backgroundColor: Colors.black26,
-    ),
-    body: Center(
-      child: Text(
-        'hello Ninjas',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey[600],
-          fontFamily: 'Rubik',
-
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-      onPressed: () {},
-      backgroundColor: Colors.red,
-    ),
-    backgroundColor: Colors.amberAccent,
-  ),
+  home: Home(),
 
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello there!"),
+        centerTitle: true,
+        backgroundColor: Colors.black26,
+      ),
+      body: Center(
+        child: Image.asset('assets/image1.jpg'),
+        // child: Image.network('https://s-media-cache-ak0.pinimg.com/736x/eb/d8/1c/ebd81ca9cde854ef1a8ccca3183e915b.jpg'),
+        // child: Text(
+        //   'hello Ninjas',
+        //   style: TextStyle(
+        //     fontSize: 20.0,
+        //     fontWeight: FontWeight.bold,
+        //     letterSpacing: 2,
+        //     color: Colors.grey[600],
+        //     fontFamily: 'Rubik',
+        //   ),
+        // ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red,
+      ),
+      backgroundColor: Colors.amberAccent,
+    );
+  }
+}
 
 
 
