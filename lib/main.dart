48a48a1103af46007688ac6812b8b0ef540c7716
Srikanth Nutigattu <srikanth.nutigattu@gmail.com>
2020-08-29 10:58:12 +0800

    added samples for different types of buttons

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,56 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.black26,
       ),
       body: Center(
-        child: Image.asset('assets/image1.jpg'),
+
+        child: IconButton(
+          onPressed: () {
+            print('clicked');
+          },
+          icon: Icon(
+            Icons.email,
+          ),
+        ),
+
+        //Buttons
+        // child: RaisedButton.icon(
+        //   onPressed: () {},
+        //   icon: Icon(
+        //     Icons.airport_shuttle
+        //   ),
+        //   label: Text('icon button'),
+        //   color: Colors.blue,
+        // ),
+
+        // child: FlatButton(
+        //   child: Text('click'),
+        //   color: Colors.lightBlue,
+        //   onPressed: () {
+        //     print('you clicked!');
+        //   } ,
+        // ),
+
+        // child: RaisedButton(
+        //   child: Text('click'),
+        //   color: Colors.lightBlue,
+        //   onPressed: () {} ,
+        // ),
+
+
+        //Using Icons
+        // child: Icon(
+        //   Icons.airport_shuttle,
+        //   color: Colors.lightBlue,
+        //   size: 50.0 ,
+        // ),
+
+
+        //Local Asset after adding it to folder and pubspec
+        // child: Image.asset('assets/image1.jpg'),
+
+        // Remote image from internet
         // child: Image.network('https://s-media-cache-ak0.pinimg.com/736x/eb/d8/1c/ebd81ca9cde854ef1a8ccca3183e915b.jpg'),
+
+        //Adding text
         // child: Text(
         //   'hello Ninjas',
         //   style: TextStyle(
